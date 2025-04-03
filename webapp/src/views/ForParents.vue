915494d748fdcf81cdddf556449525de0162adbd
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-04-03 10:15:40 +0200

    fix: text color on forParents

--- webapp/src/views/ForParents.vue
+++ webapp/src/views/ForParents.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col justify-center content-center items-center gap-20 dark:bg-zinc-950 p-20">
-    <h3 class="text-4xl font-bold text-center text-gray-100">Szülők figyelmébe</h3>
+  <div class="flex flex-col justify-center content-center items-center gap-20 text-zinc-900 dark:text-zinc-100 dark:bg-zinc-950 p-20">
+    <h3 class="text-4xl font-bold text-center ">Szülők figyelmébe</h3>
 
     <div class="flex flex-col justify-center content-center items-center gap-10">
-      <h3 class="text-3xl font-semibold text-center text-gray-200">A TANULÓK ELŐMENETELE</h3>
+      <h3 class="text-3xl font-semibold text-center ">A TANULÓK ELŐMENETELE</h3>
       <p class="text-base text-justify text-gray-300">
         Az e-kreta.hu adminisztrációs rendszer teljes körű információt biztosít tanulóink előmeneteléről.
         (Az egyéni oktatás során biztosítunk hagyományos papíralapú kommunikációs lehetőséget is “tájékoztató füzet” segítségével.)
@@ -13,12 +13,12 @@
     </div>
 
     <div class="flex flex-col justify-center content-center items-center gap-10">
-      <h3 class="text-3xl font-semibold text-center text-gray-200">TÁJÉKOZTATÓ A TÉRÍTÉSI DÍJ ÉS TANDÍJ MEGFIZETÉSÉRŐL</h3>
-      <p class="text-base text-justify text-gray-300">
+      <h3 class="text-3xl font-semibold text-center ">TÁJÉKOZTATÓ A TÉRÍTÉSI DÍJ ÉS TANDÍJ MEGFIZETÉSÉRŐL</h3>
+      <p class="text-base text-justify ">
         A beiratkozás után – az ott megadott ímélcímre – mindenkinek személyre szóló tájékoztatást küldünk a fizetési kötelezettségekről, valamint az önkéntes támogatási lehetőségekről!
         Az alábbiakban hirdetmény jelleggel szolgálunk információval.
         Köznevelési feladatot ellátó intézményegységenként a térítési díj, a tandíj, egyéb díjfizetési kötelezettség (a továbbiakban együtt: díj) jogcíme és mértéke, továbbá tanévenként, nevelési évenként az egy főre megállapított díjak mértéke, a fenntartó által adható kedvezmények, beleértve a jogosultsági és igénylési feltételeket is:
-        <ul class="list-disc pl-5 mt-4 text-gray-300">
+        <ul class="list-disc pl-5 mt-4 ">
           <li><strong>A Danubia Alapfokú Művészeti Iskola térítési éves díjai és tandíjai a 2024 / 2025. tanévben:</strong></li>
           <li><strong>A térítési díj összege 18 év alatti tanulók esetében:</strong></li>
           <ul class="list-decimal pl-5">
@@ -39,8 +39,8 @@
     </div>
 
     <div class="flex flex-col justify-center content-center items-center gap-10">
-      <h3 class="text-3xl font-semibold text-center text-gray-200">ISMERJE MEG EREDMÉNYEINKET!</h3>
-      <p class="text-base text-justify text-gray-300">
+      <h3 class="text-3xl font-semibold text-center ">ISMERJE MEG EREDMÉNYEINKET!</h3>
+      <p class="text-base text-justify ">
         Az e-kreta.hu adminisztrációs rendszer teljes körű információt biztosít tanulóink előmeneteléről.
         (Az egyéni oktatás során biztosítunk hagyományos papíralapú kommunikációs lehetőséget is “tájékoztató füzet” segítségével.)
         Ha már beiratkozott gyermeke, de Ön még nem rendelkezik e-kérta szülői fiókkal, kérjük, adja meg az e célra szánt ímélcímét a <a href="mailto:danubiaiskola@gmail.com" class="text-blue-400 underline">danubiaiskola@gmail.com</a> címen, feltüntetve az érintett tanuló, illetve tanulók nevét és tanulóazonosítóját.
@@ -49,7 +49,7 @@
     </div>
 
     <div class="flex flex-col justify-items-center content-center items-center gap-10">
-      <h2 class="text-2xl font-semibold text-gray-200">Országos, nemzetközi és egyéb szakmai bemutatók</h2>
+      <h2 class="text-2xl font-semibold ">Országos, nemzetközi és egyéb szakmai bemutatók</h2>
       <ul class="list-disc pl-5 text-gray-300">
         <li>2023.10.30. Budapest, Gitár Centrum, Bodor Máté Masterclass, Kincs János döntős (Északi Szabolcs)</li>
         <li>2023.10.23. Baja, La Grande Lessive Nemzetközi képzőművészeti akció (Majorcsicsné Ujjady Krisztina)</li>
@@ -57,21 +57,21 @@
         <li>2024.03.23. Budapest, Magyar Zene Háza, Tavaszi Fesztivál – Hegyi Zselyke fellépése (Pencz Andrea)</li>
       </ul>
 
-      <h2 class="text-2xl font-semibold text-gray-200 mt-10">Megyei, területi szakmai bemutatók</h2>
+      <h2 class="text-2xl font-semibold  mt-10">Megyei, területi szakmai bemutatók</h2>
       <ul class="list-disc pl-5 text-gray-300">
         <li>2024.05.11. Miske, Néptánc Együttesek Találkozója: Danubia AMI Nádikó ifjúsági csoport (Kővágó Zsolt)</li>
         <li>2024.04.05. Baja, Kultúrpalota, III. Tüzet viszek népdaléneklési verseny, vármegyei forduló: Hegyi Zselyke ezüst minősítés (Pencz Andrea)</li>
         <li>2024.03.10. Kecskemét, Megyei Fúvószenekari Találkozó – Danubia AMI Fúvószenekar (Nagy Gábor)</li>
       </ul>
 
-      <h2 class="text-2xl font-semibold text-gray-200 mt-10">Helyi kulturális szerepvállalás</h2>
+      <h2 class="text-2xl font-semibold  mt-10">Helyi kulturális szerepvállalás</h2>
       <ul class="list-disc pl-5 text-gray-300">
         <li>2024.08.31. Szakmár Falunap - Nádikó Ifjúsági és kisiskolás csoportok (Kővágó Zsolt)</li>
         <li>2024.06.01. Újtelek Falunap - Nádikó Ifjúsági és kisiskolás csoportok (Kővágó Zsolt)</li>
         <li>2024.05.20. Baja, Városi Ünnepség – Danubia AMI kórus és fúvószenekar (Kórusvezető: Kiss Edit, Zenekarvezető: Nagy Gábor)</li>
       </ul>
 
-      <h2 class="text-2xl font-semibold text-gray-200 mt-10">Országos, nemzetközi és egyéb versenyek</h2>
+      <h2 class="text-2xl font-semibold  mt-10">Országos, nemzetközi és egyéb versenyek</h2>
       <ul class="list-disc pl-5 text-gray-300">
         <li>2023.11.20. Debrecen, Országos gitárverseny, 2. helyezés – Kincs János</li>
         <li>2023.12.15. Pécs, Országos Fúvósverseny, 3. helyezés – Danubia AMI (Nagy Gábor)</li>
