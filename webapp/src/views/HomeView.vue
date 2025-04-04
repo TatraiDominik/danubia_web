4d21cdb3f785dc26ac3c170609136c07a859df43
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-04-05 00:30:43 +0200

    ref: more responsiveness on homeview

--- webapp/src/views/HomeView.vue
+++ webapp/src/views/HomeView.vue
@@ -32,7 +32,7 @@ const processedSvg = computed(() => {
 </script>
 
 <template>
-  <div class="w-full h-full">
+  <div class="w-full h-full bg-zinc-100 dark:bg-zinc-950">
     <div class="absolute left-0 top-0 w-full h-screen">
       <!--wave.svg-->
       <div v-html="processedSvg.wave" class="absolute w-full h-full object-cover"></div>
@@ -41,7 +41,7 @@ const processedSvg = computed(() => {
     </div>
 
 
-    <section class="relative w-full h-[94.2%]">
+    <section class="relative w-full h-full">
       <div class="lightYellow absolute left-[17%] top-1/2 w-[35%] h-[35%] z-[-1]"><!--Circle--></div>
       <div class="darkYellow absolute right-[17%] top-0 w-[35%] h-[35%] z-[-1]"><!--Circle--></div>
       <div class="relative flex flex-col justify-center content-center items-center w-full h-full gap-5">
@@ -49,9 +49,9 @@ const processedSvg = computed(() => {
         <div class="relative flex flex-col justify-center w-[60%] h-[60%] content-center items-center z-10 text-zinc-100 dark:text-zinc-100 gap-10 name">
 
           <div class="flex flex-col justify-center content-start items-start gap-2">
-            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:text-3xl text-4xl">Danubia</h3>
-            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:text-3xl text-4xl">Alapfokú</h3>
-            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:text-3xl text-4xl">Művészeti Iskola</h3>
+            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:homeText text-4xl">Danubia</h3>
+            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:homeText text-4xl">Alapfokú</h3>
+            <h3 class="2xl:text-6xl xl:text-5xl lg:text-5xl md:text-4xl sm:homeText text-4xl">Művészeti Iskola</h3>
           </div>
           <div class="w-full flex flex-row justify-center content-center items-center flex-wrap gap-5">
             <Button rank="primary" size="normal" icon-position="none" text="Képzéseink" :onClick="()=> router.push('/trainings')"></Button>
@@ -64,12 +64,12 @@ const processedSvg = computed(() => {
     </section>
 
 
-    <section class="relative w-full h-screen bg-blue-600">
+    <section class="relative w-full h-screen bg-blue-600 px-4">
       <div class="layered absolute w-[80%] h-[80%] top-[15%] left-[-10%]"></div>
-      <div class="relative flex flex-row justify-center items-center w-full h-full gap-20">
-        <div class="banner relative w-[400px] h-[500px] rounded-xl"></div>
-        <div class="flex flex-col justify-center content-start items-start gap-5 w-1/2 h-full">
-          <h3 class="text-3xl text-zinc-300 dark:text-zinc-100 w-full text-justify leading-relaxed">
+      <div class="relative flex flex-col md:flex-row justify-center items-center w-full h-full gap-5 md:gap-20 ">
+        <div class="banner relative w-2/3 md:w-1/3 h-2/6 rounded-xl"></div>
+        <div class="flex flex-col justify-center content-start items-start gap-5 md:w-1/2 w-3/4">
+          <h3 class="homeText text-zinc-300 dark:text-zinc-100 w-full text-justify leading-relaxed">
             Miként a Duna összeköti a különböző kultúrákat,
             s a danubia szalag a ruha részeit egésszé egyesíti,
             akként
@@ -81,47 +81,59 @@ const processedSvg = computed(() => {
 
 
     <section class="relative w-full h-screen overflow-hidden dark:bg-zinc-950">
-      <div class="relative flex flex-row justify-center items-center w-full h-full gap-20">
+      <div class="relative flex md:flex-row flex-col justify-center items-center w-full h-full py-4 gap-5 md:gap-20">
         <div v-html="processedSvg.blueFin" class="absolute bottom-[-75%] left-0 w-full h-full"></div>
-        <div v-html="processedSvg.lightYellowFin" class="absolute top-0 right-[-70%] w-full h-full"></div>
+        <div v-html="processedSvg.lightYellowFin" class="absolute top-0 right-[-70%] w-full h-full hidden md:block"></div>
         <div v-html="processedSvg.darkYellowFin" class="absolute bottom-[-65%] right-[-80%] w-full h-full"></div>
-        <div class="flex flex-col justify-center content-start items-start w-1/4 gap-5">
+
+
+        <div class="flex flex-col justify-center content-start items-start  md:w-1/4 w-5/6 gap-5">
           <h3 class="text-4xl dark:text-zinc-100">Iskolatörténet</h3>
-          <p class="text-lg font-[600] dark:text-zinc-100 text-justify">
+          <p class="md:text-lg text-base font-[600] dark:text-zinc-100 text-justify z-50">
             Intézményünk az országban tizenhatodik magánfenntartású zeneiskolaként, bajai fenntartói és igazgatási székhellyel a Baja környéki kistelepüléseken 1994 szeptemberében kezdte meg működését.
             A közoktatási feladatokat ellátó Magánzeneiskola azon vidéki tanulók részére jött létre, akik addig földrajzi, illetve szociális helyzetükből adódóan nem vehettek részt alapfokú zeneoktatásban.
-            1996-tól az iskola megkezdte a nem zenei (képző- és iparművészeti, színművészeti és táncművészeti) tárgyak  oktatását.
+            1996-tól az iskola megkezdte a nem zenei (képző- és iparművészeti, színművészeti és táncművészeti) tárgyak oktatását.
             A 2000/2001. tanévtől Danubia Alapfokú Művészeti Iskola néven Baján is megindult az alapfokú művészetoktatás.
           </p>
         </div>
-        <h3 class="text-4xl dark:text-zinc-100">-</h3>
+
+        <h3 class="text-4xl dark:text-zinc-100 hidden md:block">-</h3>
+
+
         <div class="flex flex-col justify-center content-center items-center gap-5 z-[100]">
-          <div class="principle w-[20rem] h-[20rem] rounded-full"><!--Principle--></div>
+          <div class="principle w-[15rem] sm:w-[18rem] md:w-[20rem] lg:w-[22rem] h-[15rem] sm:h-[18rem] md:h-[20rem] lg:h-[22rem] rounded-full bg-cover bg-center principle">
+            <!--Principle Image-->
+          </div>
           <h3 class="text-4xl dark:text-zinc-100">Petővári László</h3>
-          <h3 class="text-3xl dark:text-zinc-100">Az intézmény vezetője</h3>
+          <h3 class="homeText dark:text-zinc-100">Az intézmény vezetője</h3>
         </div>
       </div>
     </section>
 
 
+
+
     <section class="relative w-full h-screen dark:bg-zinc-950">
       <div class="relative flex flex-col justify-center items-center w-full h-full">
-        <div class="flex flex-row justify-center content-center items-center gap-20">
-          <div class="logo w-[20rem] h-[20rem]">
+
+        <div class="flex flex-col md:flex-row justify-center items-center w-full h-full gap-5 md:gap-20">
+          <div class="logo w-5/6 h-4/6 sm:w-3/6 sm:h-2/6 md:w-72 md:h-72">
             <!--Logo-->
           </div>
-          <div class="flex flex-col justify-center content-start items-start gap-20">
-            <div class="flex flex-col justify-center content-start items-start gap-5">
-              <h3 class="text-3xl">Jelentkezz bátran!</h3>
-              <h3 class="text-3xl">Eseményeinken szívesen látunk</h3>
-              <div class="flex flex-row justify-center content-center items-center gap-5">
+
+
+          <div class="flex flex-col justify-center items-center gap-20">
+            <div class="flex flex-col justify-center items-center gap-5">
+              <h3 class="homeText text-center">Jelentkezz bátran!</h3>
+              <h3 class="homeText text-center">Eseményeinken szívesen látunk</h3>
+              <div class="flex flex-row justify-center items-center gap-5">
                 <Button rank="primary" size="normal" icon-position="none" text="Képzéseink" :onClick="()=> router.push('/trainings')"></Button>
                 <Button rank="secondary" size="normal" icon-position="none" text="Events" :onClick="()=> router.push('/events')"></Button>
               </div>
             </div>
-            <div class="flex flex-col justify-center content-start items-start gap-5">
-              <h3 class="text-3xl">Danubia iroda:</h3>
-              <h3 class="text-3xl">+367012345</h3>
+            <div class="flex flex-col justify-center items-center gap-5">
+              <h3 class="homeText text-center">Danubia iroda:</h3>
+              <h3 class="homeText text-center">+367012345</h3>
               <Button rank="secondary" size="normal" icon-position="none" text="Kapcsolatok" :onClick="()=> router.push('/contacts')"></Button>
             </div>
           </div>
@@ -170,7 +182,10 @@ const processedSvg = computed(() => {
 .logo{
   background-image: url("../assets/pics/logo.png");
   background-position: center;
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
+}
+.homeText{
+  @apply text-xl sm:text-2xl md:text-3xl xl:text-3xl 2xl:text-3xl text-zinc-900 dark:text-zinc-100 z-50
 }
 </style>
